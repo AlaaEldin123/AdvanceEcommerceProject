1144626a8c28f340c8ecba5c8b25964321b82398
Your Name <you@example.com>
2021-05-23 15:16:28 +0300

    select multiImages with js

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -333,10 +333,11 @@
 				   <div class="form-group">
 			<h5>Main Thambail <span class="text-danger">*</span></h5>
 			<div class="controls">
-	 <input type="file" name="product_thambnail" class="form-control" required="">
+	 <input type="file" name="product_thambnail" class="form-control" onChange="mainThamUrl(this)" required="" >
      @error('product_thambnail') 
 	 <span class="text-danger">{{ $message }}</span>
 	 @enderror
+	 <img src="" id="mainThmb">
 	 		 </div>
 		</div>
 				
@@ -348,10 +349,12 @@
 				  <div class="form-group">
 			<h5>Multiple Image <span class="text-danger">*</span></h5>
 			<div class="controls">
-	 <input type="file" name="multi_img[]" class="form-control" required="">
+	 <input type="file" name="multi_img[]" class="form-control" required="" multiple="" id="multiImg">
      @error('multi_img') 
 	 <span class="text-danger">{{ $message }}</span>
 	 @enderror
+
+	 <div class="row" id="preview_img">	 </div>
 	 		 </div>
 		</div>
 								</div> <!-- end col md 4 -->
@@ -397,9 +400,6 @@
 		</div>
 				
 			</div> <!-- end col md 6 -->
-
-
-		
 			
 		</div> <!-- end 7th row  -->
 
@@ -513,6 +513,7 @@
                     type:"GET",
                     dataType:"json",
                     success:function(data) {
+                       $('select[name="subsubcategory_id"]').html('');
                        var d =$('select[name="subcategory_id"]').empty();
                           $.each(data, function(key, value){
                               $('select[name="subcategory_id"]').append('<option value="'+ value.id +'">' + value.subcategory_name_en + '</option>');
@@ -548,5 +549,53 @@
 
     });
     </script>
+
+
+<script type="text/javascript">
+	function mainThamUrl(input){
+		if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			reader.onload = function(e){
+				$('#mainThmb').attr('src',e.target.result).width(80).height(80);
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	}	
+</script>
+
+<script>
+ 
+  $(document).ready(function(){
+   $('#multiImg').on('change', function(){ //on file input change
+      if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+      {
+          var data = $(this)[0].files; //this file data
+           
+          $.each(data, function(index, file){ //loop though each file
+              if(/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)){ //check supported file type
+                  var fRead = new FileReader(); //new filereader
+                  fRead.onload = (function(file){ //trigger function on successful read
+                  return function(e) {
+                      var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(80)
+                  .height(80); //create image element 
+                      $('#preview_img').append(img); //append image to output element
+                  };
+                  })(file);
+                  fRead.readAsDataURL(file); //URL representing the file's data.
+              }
+          });
+           
+      }else{
+          alert("Your browser doesn't support File API!"); //if File API is absent
+      }
+   });
+  });
+   
+  </script>
+
+
+
+
+
 
 @endsection
