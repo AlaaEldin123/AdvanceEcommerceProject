ed2ec19a16c44799d95f8e83d14938c4a2132e2e
ALAA ELDIN ALHALLAK <alaaeldinalhallak@gmail.com>
2021-08-25 02:14:18 +0300

    LoadMore Product With Ajax Part 2

--- resources/views/frontend/product/subcategory_view.blade.php
+++ resources/views/frontend/product/subcategory_view.blade.php
@@ -4,6 +4,7 @@
 Subcategory Product
 @endsection
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 
@@ -294,82 +295,10 @@ Subcategory Product
         <div class="search-result-container ">
           <div id="myTabContent" class="tab-content category-list">
             <div class="tab-pane active " id="grid-container">
-              <div class="category-product">
-                <div class="row">
+              <div class="category-product" >
+                <div class="row" id="grid_view_product">
 
-
-
-@foreach($products as $product)
-  <div class="col-sm-6 col-md-4 wow fadeInUp">
-    <div class="products">
-      <div class="product">
-        <div class="product-image">
-          <div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
-          <!-- /.image -->
-
-           @php
-        $amount = $product->selling_price - $product->discount_price;
-        $discount = ($amount/$product->selling_price) * 100;
-        @endphp
-
-          <div>
-            @if ($product->discount_price == NULL)
-            <div class="tag new"><span>new</span></div>
-            @else
-            <div class="tag hot"><span>{{ round($discount) }}%</span></div>
-            @endif
-          </div>
-
-
-        </div>
-        <!-- /.product-image -->
-
-        <div class="product-info text-left">
-          <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}">
-            @if(session()->get('language') == 'arabic') {{ $product->product_name_ar }} @else {{ $product->product_name_en }} @endif</a></h3>
-          <div class="rating rateit-small"></div>
-          <div class="description"></div>
-
-
-@if ($product->discount_price == NULL)
-<div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span>   </div>
-
-@else
-
-<div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">$ {{ $product->selling_price }}</span> </div>
-@endif
-
-
-
-
-          <!-- /.product-price -->
-
-        </div>
-        <!-- /.product-info -->
-        <div class="cart clearfix animate-effect">
-          <div class="action">
-            <ul class="list-unstyled">
-              <li class="add-cart-button btn-group">
-                <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-              </li>
-              <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-              <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
-            </ul>
-          </div>
-          <!-- /.action -->
-        </div>
-        <!-- /.cart -->
-      </div>
-      <!-- /.product -->
-
-    </div>
-    <!-- /.products -->
-  </div>
-  <!-- /.item -->
-  @endforeach
-
-
+@include('frontend.product.grid_view_product')
 
 
 
@@ -397,85 +326,11 @@ Subcategory Product
 
 
             <div class="tab-pane "  id="list-container">
-              <div class="category-product">
+              <div class="category-product" id="list_view_product">
 
 
 
- @foreach($products as $product)
-<div class="category-product-inner wow fadeInUp">
-  <div class="products">
-    <div class="product-list product">
-      <div class="row product-list-row">
-        <div class="col col-sm-4 col-lg-4">
-          <div class="product-image">
-            <div class="image"> <img src="{{ asset($product->product_thambnail) }}" alt=""> </div>
-          </div>
-          <!-- /.product-image -->
-        </div>
-        <!-- /.col -->
-        <div class="col col-sm-8 col-lg-8">
-          <div class="product-info">
-            <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en ) }}">
-              @if(session()->get('language') == 'arabic') {{ $product->product_name_ar }} @else {{ $product->product_name_en }} @endif</a></h3>
-            <div class="rating rateit-small"></div>
-
-
-            @if ($product->discount_price == NULL)
-            <div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span>  </div>
-            @else
-<div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">$ {{ $product->selling_price }}</span> </div>
-            @endif
-
-            <!-- /.product-price -->
-            <div class="description m-t-10">
-              @if(session()->get('language') == 'arabic') {{ $product->short_descp_ar }} @else {{ $product->short_descp_en }} @endif</div>
-            <div class="cart clearfix animate-effect">
-              <div class="action">
-                <ul class="list-unstyled">
-                  <li class="add-cart-button btn-group">
-                    <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                    <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-                  </li>
-                  <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-                  <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
-                </ul>
-              </div>
-              <!-- /.action -->
-            </div>
-            <!-- /.cart -->
-
-          </div>
-          <!-- /.product-info -->
-        </div>
-        <!-- /.col -->
-      </div>
-
-
-
-         @php
-        $amount = $product->selling_price - $product->discount_price;
-        $discount = ($amount/$product->selling_price) * 100;
-        @endphp
-
-                      <!-- /.product-list-row -->
-                      <div>
-            @if ($product->discount_price == NULL)
-            <div class="tag new"><span>new</span></div>
-            @else
-            <div class="tag hot"><span>{{ round($discount) }}%</span></div>
-            @endif
-          </div>
-
-
-
-                    </div>
-                    <!-- /.product-list -->
-                  </div>
-                  <!-- /.products -->
-                </div>
-                <!-- /.category-product-inner -->
-    @endforeach
-
+ @include('frontend.product.list_view_product')
 
 
  <!--            //////////////////// Product List View END ////////////// -->
@@ -497,7 +352,6 @@ Subcategory Product
             <div class="text-right">
               <div class="pagination-container">
                 <ul class="list-inline list-unstyled">
-                  {{ $products->links()  }}
                 </ul>
                 <!-- /.list-inline -->
               </div>
@@ -580,31 +434,45 @@ Subcategory Product
 
 
 
-<script type="text/javascript">
-  function loadmoreProduct(page){
-    $.ajax({
-      type:"get",
-      url:"?page="+page,
-      beforeSend:function(response){
-        $('.ajax-loadmore-product').show();
-      }
+<script>
+    function loadmoreProduct(page){
+      $.ajax({
+        type: "get",
+        url: "?page="+page,
+        beforeSend: function(response){
+          $('.ajax-loadmore-product').show();
+        }
+      })
 
-
-    })
-
-  }  
-
-  var page = 1;
-  $(window).scroll(function(){
-    if($(window).scrollTop() +$(window).height() >= $(document).height()){
-      page ++;
-
+      .done(function(data){
+        if (data.grid_view == " " || data.list_view == " ") {
+          return;
+        }
+         $('.ajax-loadmore-product').hide();
+         $('#grid_view_product').append(data.grid_view);
+         $('#list_view_product').append(data.list_view);
+      })
+      .fail(function(){
+        alert('Something Went Wrong');
+      })
     }
 
-  })
+
+      
+    }
 
 
+
+      var page = 1;
+    $(window).scroll(function (){
+      if ($(window).scrollTop() +$(window).height() >= $(document).height()){
+        page ++;
+        loadmoreProduct(page);
+      }
+    });
 </script>
+
+
 
 
 
